e42afd322c09b89459f0fd5fb70c78a83c688dcb
Alexey Gavrilov <agavrilov@telenordigital.com>
2015-06-27 15:23:16 +0200

    Fix input range selector

--- js/input.js
+++ js/input.js
@@ -1,5 +1,5 @@
 $(function() {
-	$('input[type="range"').on("input", function(x, y) {
+	$('input[type="range"]').on("input", function(x, y) {
 			var input = $('input[id="' + this.id 
 				+ '"][type="number"]');
 			input.val(this.value);
